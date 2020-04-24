910378c6be8f44780e1beb3755b6d7ef0f1ce657
Gareth Rogers <gareth@garethrogers.net>
2020-04-24 08:58:46 +0100

    SIgn in with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:297647434003:web:654670c0d69e085a96d2e6"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if(!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if(!snapShot.exists) {
+		const {displayName, email} = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			})
+		} catch(error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
+}
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
