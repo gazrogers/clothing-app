b0fb0bc8c716db5df43b7fe686d05deaec87a46a
Gareth Rogers <gareth@garethrogers.net>
2020-04-26 17:14:09 +0100

    Add loading anim, get collection data from DB

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,33 +11,63 @@ const config = {
     messagingSenderId: "297647434003",
     appId: "1:297647434003:web:654670c0d69e085a96d2e6"
 }
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if(!userAuth) return;
+    if(!userAuth) return;
 
-	const userRef = firestore.doc(`users/${userAuth.uid}`);
-	const snapShot = await userRef.get();
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
 
-	if(!snapShot.exists) {
-		const {displayName, email} = userAuth;
-		const createdAt = new Date();
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
 
-		try {
-			await userRef.set({
-				displayName,
-				email,
-				createdAt,
-				...additionalData
-			})
-		} catch(error) {
-			console.log('error creating user', error.message);
-		}
-	}
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.log('error creating user', error.message);
+        }
+    }
 
-	return userRef;
+    return userRef;
 }
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef= firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit(); 
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const {title, items} = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    return transformedCollection.reduce((acc, collection) => {
+        acc[collection.title.toLowerCase()] = collection;
+        return acc;
+    }, {});
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
